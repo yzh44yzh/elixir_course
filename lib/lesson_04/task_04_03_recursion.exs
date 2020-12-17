e673fc591b2e1a84e33ddba60f19a6d5045e1386
Yuriy Zhloba <y_zhloba@wargaming.net>
2020-12-17 21:40:28 +0300

    tasks for 03 04 recursion

--- lib/lesson_04/task_04_03_recursion.exs
+++ lib/lesson_04/task_04_03_recursion.exs
@@ -11,4 +11,15 @@ defmodule Lesson_04.Task_04_03_Recursion do
     max(head, list_max(tail))
   end
 
+  def range(from, to) when from <= to do
+    [from | range(from + 1, to)]
+  end
+  def range(_, _), do: []
+
+  def swap_pair([]), do: []
+  def swap_pair([_]), do: raise "Can't swap a list with an odd number of elements"
+  def swap_pair([a, b | tail]) do
+    [b, a | swap_pair(tail)]
+  end
+
 end
