8acfd4c24b529dbd2c14675facd716e8b37bdb58
Yuriy Zhloba <y_zhloba@wargaming.net>
2021-03-29 19:28:18 +0300

    lesson 06 done with 06_05_map

---
+++ lesson_06/event/lib/best_song.ex
@@ -0,0 +1,19 @@
+defmodule BestSong do
+
+  def yura_favorite_songs() do
+    %{
+      "Bel Suono" => "Libertango",
+      "The Manhattan Transfer" => "The Offbeat of Avenues",
+      "Péter Bence" => "Despacito"
+    }
+  end
+
+  def tihon_favorite_songs() do
+    %{
+      "The Manhattan Transfer" => "The Junction",
+      "Péter Bence" => "Africa",
+      "Electric Light Orchestra" => "Mr. Blue Sky"
+    }
+  end
+
+end
