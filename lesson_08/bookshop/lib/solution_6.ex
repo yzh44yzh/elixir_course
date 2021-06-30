f675f80dbe66ae06811569439a2e932d009adad0
Yuriy Zhloba <y_zhloba@wargaming.net>
2021-06-30 18:36:31 +0300

    lesson 08, do-notation, solution 6

---
+++ lesson_08/bookshop/lib/solution_6.ex
@@ -0,0 +1,28 @@
+defmodule Solution6 do
+
+  alias BookShop.Validator, as: V
+
+  @spec main() :: {:ok, BookShop.Order.t} | {:error, term}
+  def main() do
+    with(
+      {:ok, %{
+          "cat" => cat0,
+          "address" => address0,
+          "books" => books0,
+       }} <- V.validate_incoming_data(BookShop.test_data),
+      
+      {:ok, cat} <- V.validate_cat(cat0),
+      {:ok, address} <- V.validate_address(address0),
+      
+      {:ok, books} <- Enum.map(
+        books0,
+        fn(%{"title" => title, "author" => author})->
+          BookShop.get_book(title, author)
+        end)
+        |> FP.sequence()
+    ) do
+      BookShop.create_order(cat, address, books)
+    end
+  end
+
+end
