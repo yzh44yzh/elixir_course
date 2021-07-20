ff5227aec86de69574f43f2b82ef10a5604717c7
Yuriy Zhloba <y_zhloba@wargaming.net>
2021-07-20 19:49:02 +0300

    09_02 mailbox

--- lesson_09/09_02_mailbox.exs
+++ lesson_09/09_02_mailbox.exs
@@ -7,5 +7,19 @@ defmodule Lesson_09.Task_02_Mailbox do
       msg -> IO.puts("got unknown msg #{inspect msg}")
     end
   end
+
+  def check_mailbox(wait_time) do
+    receive do
+      msg -> IO.puts("got msg #{inspect msg}")
+    after wait_time -> IO.puts("no messages after #{wait_time} ms")
+    end
+  end
+
+  def check_for_42() do
+    receive do
+      42 -> IO.puts("got 42")
+    after 500 -> :ok
+    end
+  end
   
 end
