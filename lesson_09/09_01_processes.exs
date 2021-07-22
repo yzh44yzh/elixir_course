7f2de13e020474b6adf34aad3fd0aa608a505a5f
Yuriy Zhloba <y_zhloba@wargaming.net>
2021-07-22 15:51:11 +0300

    09_03

--- lesson_09/09_01_processes.exs
+++ lesson_09/09_01_processes.exs
@@ -1,7 +1,7 @@
 defmodule Lesson_09.Task_01_Processes do
 
   def run_processes(num) do
-    for id <- 0..num, do: spawn(__MODULE__, :start, [id])
+    for id <- 1..num, do: spawn(__MODULE__, :start, [id])
   end
 
   def start(id) do
---
+++ lesson_09/09_03_link.exs
@@ -0,0 +1,18 @@
+defmodule Lesson_09.Task_03_Link do
+
+  def run_and_exit(num) do
+    # for id <- 1..num, do: spawn(__MODULE__, :start_and_exit, [id])
+    for id <- 1..num, do: spawn_link(__MODULE__, :start_and_exit, [id])
+  end
+
+  def start_and_exit(id) do
+    IO.puts("Process id:#{id} pid:#{inspect self()} started")
+    if id == 3 do
+      IO.puts("Process id:#{id} exits")
+      exit(:some_reason)
+    end
+    Process.sleep(Enum.random(10..500))
+    IO.puts("Process id:#{id} pid:#{inspect self()} stopped")
+  end
+    
+end
