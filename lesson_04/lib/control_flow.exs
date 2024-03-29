8025ae986e9b716be294314e8fa6957b3b7c7bcd
Yury Zhloba <yury.zhloba@ecomcharge.com>
2024-03-29 18:59:02 +0300

    improve 04-03

--- lesson_04/lib/control_flow.exs
+++ lesson_04/lib/control_flow.exs
@@ -1,5 +1,6 @@
 defmodule ControlFlow do
 
+  # inner case
   def handle(animal, action) do
     case animal do
       {:dog, name} ->
@@ -20,6 +21,7 @@ defmodule ControlFlow do
     end
   end
 
+  # flat case
   def handle2(animal, action) do
     case {animal, action} do
       {{:dog, name}, :feed} -> IO.puts("feed the dog #{name}")
@@ -31,19 +33,24 @@ defmodule ControlFlow do
     end
   end
 
+  # function clause instead of case
   def handle3({:dog, name}, :feed), do: IO.puts("feed the dog #{name}")
   def handle3({:dog, name}, :pet), do: IO.puts("pet the dog #{name}")
   def handle3({:cat, name}, :feed), do: IO.puts("feed the cat #{name}")
   def handle3({:cat, name}, :pet), do: IO.puts("pet the cat #{name}")
   def handle3({animal, name}, action) do
-    IO.puts("do #{action} with the #{animal} #{name}")
+    IO.puts("do '#{action}' with the #{animal} #{name}")
   end
+  # def handle4(anything, whatever) do
+  #   IO.puts("do '#{inspect(anything)}' with #{inspect(whatever)}")
+  # end
 
-  # TODO
+  # guards
   def handle_({:dog, name, age}, :feed) when age > 10, do: IO.puts("give more food to the dog #{name}")
   def handle_({:dog, name, _age}, :feed), do: IO.puts("feed the dog #{name}")
   def handle_({:dog, name, _age}, :pet), do: IO.puts("pet the dog #{name}")
 
+  # guards with case
   def handle4(animal) do
     case animal do
       {:dog, name, age} when age > 10 -> IO.puts("#{name} is a dog older than 10")
@@ -53,6 +60,7 @@ defmodule ControlFlow do
     end
   end
 
+  # guards with function clauses
   def handle5({:dog, name, age}) when age > 10 do
     IO.puts("#{name} is a dog older than 10")
   end
@@ -66,13 +74,15 @@ defmodule ControlFlow do
     IO.puts("#{name} is a 10 years old or younger cat")
   end
 
+  # crash in guards
   def handle6(num) when 10 / num > 2 do
     IO.puts("clause 1")
   end
-  def handle6(num) do
+  def handle6(_num) do
     IO.puts("clause 2")
   end
 
+  # cond
   def handle7(num) do
     cond do
       num > 10 -> IO.puts("more than 10")
@@ -82,30 +92,31 @@ defmodule ControlFlow do
 
 end
 
-animal1 = {:cat, "Tihon"}
-animal2 = {:dog, "Woof"}
+# animal_1 = {:cat, "Tihon"}
+# animal_2 = {:dog, "Woof"}
+# animal_3 = {:rat, "Bet"}
 
-alias ControlFlow, as: CF
+# alias ControlFlow, as: CF
 
-CF.handle(animal1, :feed)
-CF.handle(animal2, :pet)
+# CF.handle(animal1, :feed)
+# CF.handle(animal2, :pet)
 
-CF.handle2(animal2, :feed)
-CF.handle2(animal1, :pet)
+# CF.handle2(animal2, :feed)
+# CF.handle2(animal1, :pet)
 
-CF.handle3(animal1, :feed)
-CF.handle3(animal2, :feed)
+# CF.handle3(animal1, :feed)
+# CF.handle3(animal2, :feed)
 
-CF.handle4({:dog, "Woof", 14})
-CF.handle4({:dog, "Woof", 9})
+# CF.handle4({:dog, "Woof", 14})
+# CF.handle4({:dog, "Woof", 9})
 
-CF.handle5({:cat, "Tihon", 12})
-CF.handle5({:cat, "Tihon", 10})
+# CF.handle5({:cat, "Tihon", 12})
+# CF.handle5({:cat, "Tihon", 10})
 
-CF.handle6(0)
-CF.handle6(5)
-CF.handle6(3)
+# CF.handle6(0)
+# CF.handle6(5)
+# CF.handle6(3)
 
-CF.handle7(20)
-CF.handle7(8)
-CF.handle7(3)
+# CF.handle7(20)
+# CF.handle7(8)
+# CF.handle7(3)
