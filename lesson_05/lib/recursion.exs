b31e51c3441fd9ab5ce1e3f6221f6bc75998af49
Yury Zhloba <yury.zhloba@ecomcharge.com>
2024-05-02 19:00:33 +0300

    05-03

--- lesson_05/lib/recursion.exs
+++ lesson_05/lib/recursion.exs
@@ -1,23 +1,37 @@
 defmodule Recursion do
 
   def len([]), do: 0
+
   def len([_head | tail]) do
     1 + len(tail)
   end
 
   def list_max([]), do: nil
+
   def list_max([elem]), do: elem
+
   def list_max([head | tail]) do
     max(head, list_max(tail))
+  end
+
+  def set_value([], _position, _value), do: []
+
+  def set_value([_head | tail], 1, value), do: [value | tail]
+
+  def set_value([head | tail], position, value) do
+    [head | set_value(tail, position - 1, value)]
   end
 
   def range(from, to) when from <= to do
     [from | range(from + 1, to)]
   end
+
   def range(_, _), do: []
 
   def swap_pair([]), do: []
+
   def swap_pair([_]), do: raise "Can't swap a list with an odd number of elements"
+
   def swap_pair([a, b | tail]) do
     [b, a | swap_pair(tail)]
   end
