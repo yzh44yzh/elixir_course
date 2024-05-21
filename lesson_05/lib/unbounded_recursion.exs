b58f3d0c05c9537bd2c745166520d77b3a1427d1
Yury Zhloba <yury.zhloba@ecomcharge.com>
2024-05-21 14:35:48 +0300

    05-06

--- lesson_05/lib/unbounded_recursion.exs
+++ lesson_05/lib/unbounded_recursion.exs
@@ -1,26 +1,31 @@
 defmodule UnboundedRecursion do
 
+  def browse() do
+    browse("/home/yuri/p/elixir_course/lesson_05")
+  end
+
   def browse(path) do
-    browse(path, "", [])
+    browse(path, [])
   end
 
-  defp browse([], _parent, acc), do: acc
-
-  defp browse([path | tail], parent, acc) do
-    new_acc = browse(path, parent, acc)
-    browse(tail, parent, new_acc)
-  end
-
-  defp browse(path, parent, acc) when is_binary(path) do
-    full_path = Path.join(parent, path)
+  defp browse(path, acc) do
     cond do
-      File.regular?(full_path) ->
-        [full_path | acc]
-      File.dir?(full_path) ->
-        {:ok, items} = File.ls(full_path)
-        new_acc = Enum.sort(items) |> browse(full_path, acc)
-        [full_path | new_acc]
-      end
+      File.regular?(path) ->
+        [path | acc]
+
+      File.dir?(path) ->
+        {:ok, items} = File.ls(path)
+        new_acc = browse_items(items, path, acc)
+        [path | new_acc]
+    end
+  end
+
+  defp browse_items([], _parent, acc), do: acc
+
+  defp browse_items([item | items], parent, acc) do
+    full_path = Path.join(parent, item)
+    new_acc = browse(full_path, acc)
+    browse_items(items, parent, new_acc)
   end
 
 end
