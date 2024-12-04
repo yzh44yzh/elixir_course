cab15e49147b3595e98971c45558d7d33bf7cc64
dmitry-sidorov <jimmaggot@mail.ru>
2024-12-05 01:37:36 +0300

    Add parse task function

--- project_01/work_report/lib/markdown_parser.ex
+++ project_01/work_report/lib/markdown_parser.ex
@@ -1,8 +1,38 @@
 defmodule WorkReport.MarkdowParser do
+  alias WorkReport.Model.Task
   @behaviour WorkReport.Parser
 
   @impl WorkReport.Parser
   def parse_report(report, opts) do
-    report
+    [month | days] =
+      report
+      |> String.split("\n\n")
+
+    days
+    |> Enum.map(fn day -> String.split(day, "\n") end)
+    |> Enum.map(fn [day | tasks] ->
+      [
+        day |> String.split(" "),
+        tasks
+        |> Enum.map(&parse_task/1)
+        |> Enum.reject(&is_nil/1)
+      ]
+    end)
+  end
+
+  @spec parse_task(task :: binary()) :: Task.t()
+  defp parse_task(task) do
+    task |> dbg()
+    regexp = ~r/^(\[\w+\])\s(.+)\s\-\s([\w+|\s]+)$/
+    result = Regex.scan(regexp, task)
+
+    case result do
+      [] ->
+        nil
+
+      [[_, category, description, time_spent]] ->
+        # TODO: convert time to minutes
+        %Task{category: category, description: description, time_spent: time_spent}
+    end
   end
 end
