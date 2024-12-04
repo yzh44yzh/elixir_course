d9f446ea6ccfe93cd1992dd625f6f6f53869c45e
dmitry-sidorov <jimmaggot@mail.ru>
2024-12-05 01:37:36 +0300

    Add behaviour to implementation scheme

--- project_01/work_report/lib/report.ex
+++ project_01/work_report/lib/report.ex
@@ -2,12 +2,15 @@ defmodule Report do
   @moduledoc """
     Report model
 
-    report.md -> parse() -> %Month{}
+    // MarkdownParser module implements behaviour from Parser module.
+    report.md -> Parser.parse_report(report_file, MarkdownParser) -> %Month{}
 
-    %Month{} -> build_report(day: 5) -> %DayReport{}
-    %Month{} -> build_report(month: 2) -> %MonthReport{}
-    %Month{} -> build_report(month: 2, day: 5) -> {%MonthReport{}, %DayReport{}}
+    %Month{} -> build_report(day: 5) -> {_, %DayReport{}} // %Report{month: nil, day: %DayReport{}}
+    %Month{} -> build_report(month: 2) -> {%MonthReport{}, _} // %Report{month: %MonthReport{}, day: nil}
+    %Month{} -> build_report(month: 2, day: 5) -> {%MonthReport{}, %DayReport{}} // %Report{month: %MonthReport{}, day: %DayReport{}}
 
+    // TerminalPrinter module implements behaviour from Printer module.
+    %Report{} -> Printer.print_report(%Report{}, TerminalPrinter)
   """
 
   defmodule Month do
