206f9944b24a1f2400a7c29c5b456c6f8c1e6327
dmitry-sidorov <jimmaggot@mail.ru>
2024-12-05 01:37:37 +0300

    Update day parser

--- project_01/work_report/lib/markdown_parser.ex
+++ project_01/work_report/lib/markdown_parser.ex
@@ -1,5 +1,5 @@
 defmodule WorkReport.MarkdownParser do
-  alias WorkReport.Model.Task
+  alias WorkReport.Model.{Day, Task}
   @behaviour WorkReport.Parser
 
   @minutes_in_one_hour 60
@@ -9,9 +9,11 @@ defmodule WorkReport.MarkdownParser do
     [month | days] =
       report
       |> String.split("\n\n")
+      |> dbg()
 
     days
     |> Enum.map(fn day -> String.split(day, "\n") end)
+    |> dbg()
     |> Enum.map(fn [day | tasks] ->
       [
         day |> String.split(" "),
@@ -20,6 +22,33 @@ defmodule WorkReport.MarkdownParser do
         |> Enum.reject(&is_nil/1)
       ]
     end)
+  end
+
+  @spec parse_day(full_day_string :: binary()) :: Day.t()
+  def parse_day(full_day_string) do
+    [day_string | task_string_list] = String.split(full_day_string, "\n")
+
+    parse_day_string(day_string) |> parse_task_list(task_string_list)
+  end
+
+  @spec parse_day_string(day_string :: binary()) :: Day.t()
+  def parse_day_string(day_string) do
+    regexp = ~r/^##\s(?<number>\d+)\s(?<title>\w+)$/
+
+    case Regex.named_captures(regexp, day_string) do
+      %{"number" => number, "title" => title} ->
+        %Day{number: String.to_integer(number), title: title}
+
+      nil ->
+        nil
+    end
+  end
+
+  @spec parse_task_list(day :: Day.t(), task_string_list :: [binary()]) :: Day.t()
+  def parse_task_list(day, task_string_list) do
+    tasks = Enum.map(task_string_list, &parse_task/1) |> Enum.reject(&is_nil/1)
+
+    Map.put(day, :tasks, tasks)
   end
 
   @spec parse_task(task :: binary()) :: Task.t()
--- project_01/work_report/test/markdown_parser_test.exs
+++ project_01/work_report/test/markdown_parser_test.exs
@@ -1,7 +1,8 @@
 defmodule MarkdownParserTest do
   use ExUnit.Case
 
-  alias WorkReport.{MarkdownParser, Model.Task}
+  alias WorkReport.{MarkdownParser}
+  alias WorkReport.Model.{Day, Task}
 
   # TODO: Add StreamData package to tests for property-based testing
 
@@ -19,6 +20,59 @@ defmodule MarkdownParserTest do
       assert MarkdownParser.parse_task("Some Do something useful - 30r") == nil
     end
   end
+
+  describe "parse_day_string" do
+    test "should parse a day string to map" do
+      assert MarkdownParser.parse_day_string("## 3 mon") == %Day{
+               number: 3,
+               title: "mon",
+               tasks: []
+             }
+    end
+  end
+
+  describe "parse_task_list" do
+    test "should parse task string list" do
+      assert MarkdownParser.parse_task_list(%Day{number: 1, title: "mon"}, [
+               "[SOME] Test title - 15m",
+               "[DEV] Do some useful stuff - 2h",
+               "[COM] Daily meeting with indians - 5h 30m"
+             ]) == %Day{
+               number: 1,
+               title: "mon",
+               tasks: [
+                 %Task{category: "SOME", description: "Test title", time_spent: 15},
+                 %Task{category: "DEV", description: "Do some useful stuff", time_spent: 120},
+                 %Task{
+                   category: "COM",
+                   description: "Daily meeting with indians",
+                   time_spent: 330
+                 }
+               ]
+             }
+    end
+  end
+
+  # describe "parse_day" do
+  test "should" do
+    assert MarkdownParser.parse_day(
+             "## 3 mon\n[DEV] Review Shitty Pull Requests - 30m\n[COMM] Daily Meeting with arabs - 15m\n[DEV] Implement cool feature for legacy monster - 4h\n"
+           ) == %Day{
+             number: 3,
+             title: "mon",
+             tasks: [
+               %Task{category: "DEV", description: "Review Shitty Pull Requests", time_spent: 30},
+               %Task{category: "COMM", description: "Daily Meeting with arabs", time_spent: 15},
+               %Task{
+                 category: "DEV",
+                 description: "Implement cool feature for legacy monster",
+                 time_spent: 240
+               }
+             ]
+           }
+  end
+
+  # end
 
   describe "parse_time" do
     test "should parse time string" do
