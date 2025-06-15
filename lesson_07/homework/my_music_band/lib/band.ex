47cc54a178f880b0acc5f385ba5a47fb74ddc82a
Sergei Bylevskiy <sergeibylewskii@gmail.com>
2025-06-15 19:48:02 +0300

    done homework for lesson_07

--- lesson_07/homework/my_music_band/lib/band.ex
+++ lesson_07/homework/my_music_band/lib/band.ex
@@ -1,9 +1,29 @@
 defmodule MyMusicBand.Band do
+  alias MyMusicBand.{Vocalist, Guitarist, Drummer}
 
-  def init(), do: nil
+  defstruct players: []
 
-  def add_player(arg1, arg2), do: nil
+  @type t() :: %__MODULE__{
+          players: list(Guitarist.t() | Drummer.t() | Vocalist.t())
+        }
 
-  def next(arg), do: nil
+  @spec init() :: t()
+  def init(), do: %__MODULE__{}
 
-end 
+  @spec add_player(t(), Guitarist.t() | Drummer.t() | Vocalist.t()) :: t()
+  def add_player(band, player) do
+    %{band | players: band.players ++ [player]}
+  end
+
+  @spec next(t()) :: {list(atom), t()}
+  def next(band) do
+    {sounds, updated_players} =
+      Enum.map(band.players, fn player ->
+        player.__struct__.next(player)
+      end)
+      |> Enum.unzip()
+
+    new_band = %{band | players: updated_players}
+    {sounds, new_band}
+  end
+end
--- lesson_07/homework/my_music_band/lib/vocalist.ex
+++ lesson_07/homework/my_music_band/lib/vocalist.ex
@@ -1,7 +1,55 @@
 defmodule MyMusicBand.Vocalist do
+  alias MyMusicBand.Model.Sound
+  import Sound, only: [is_vocal: 1]
 
-  def init(arg), do: nil
+  defstruct [:sounds, :current_stream]
 
-  def next(arg), do: nil
+  @type t() :: %__MODULE__{
+          sounds: [Sound.vocal_sound()],
+          current_stream: Enumerable.t()
+        }
 
-end 
+  @spec init([Sound.vocal_sound()]) :: {:ok, t()} | {:error, [{integer(), :atom}]}
+  def init(sounds) do
+    case validate_sounds(sounds) do
+      ^sounds ->
+        {:ok,
+         %__MODULE__{
+           sounds: sounds,
+           current_stream: Stream.cycle(sounds)
+         }}
+
+      {:error, error} ->
+        {:error, error}
+    end
+  end
+
+  @spec next(t()) :: {Sound.vocal_sound(), t()}
+  def next(%__MODULE__{current_stream: stream} = drummer) do
+    {sound, new_stream} = get_sound(stream)
+    drummer = %__MODULE__{drummer | current_stream: new_stream}
+    {sound, drummer}
+  end
+
+  defp get_sound(stream) do
+    case stream |> Stream.take(1) |> Enum.to_list() do
+      [sound] ->
+        {sound, stream |> Stream.drop(1)}
+
+      _ ->
+        {nil, stream}
+    end
+  end
+
+  defp validate_sounds(sounds) do
+    case Enum.with_index(sounds, 1)
+         |> Enum.filter(fn {sound, _index} -> !is_vocal(sound) end)
+         |> Enum.map(fn {sound, index} -> {index, sound} end) do
+      [] ->
+        sounds
+
+      errors ->
+        {:error, errors}
+    end
+  end
+end
